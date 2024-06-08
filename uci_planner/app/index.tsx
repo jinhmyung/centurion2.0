bb658a1d18de4ef80259837c09dda59ada44a5ea
jsonyun <69773462+jsonyun@users.noreply.github.com>
2024-06-08 00:18:04 -0700

    add content to GE

--- uci_planner/app/index.tsx
+++ uci_planner/app/index.tsx
@@ -33,8 +33,73 @@ export default function App() {
           </div>
           <div className="section-content">
             {activeSection === 'GE' ? (
-              <div>General Education Content
-                <Card text="Sample Card Text" />
+              <div>
+                <h3>General Education Content</h3>
+                <h4>Ia. Lower Division Writing (2)</h4>
+                <div className="cards-row">
+                  <Card text="Writing 50" />
+                  <Card text="Writing 60" />
+                </div>
+                <h4>Ib. Upper Division Writing (1)</h4>
+                <div className="cards-row">
+                  <Card text="ICS 139W" />
+                </div>
+                <h4>II. Science and Technology (3)</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="BioSci 1A" />
+                  <Card text="BioSci 6" />
+                  <Card text="BioSci 8" />
+                </div>
+                <h4>III. Social and Behavioral Sciences (3)</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="AFAM 40A" />
+                  <Card text="AFAM 40B" />
+                  <Card text="AFAM 40C" />
+                </div>
+                <h4>IV. Arts and Humanities (3)</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="AFAM 40A" />
+                  <Card text="AFAM 40B" />
+                  <Card text="AFAM 40C" />
+                </div>
+                <h4>V. Quantitative, Symbolic, and Computational Reasoning (3)</h4>
+                <p>You are required to take one course in each of the subcategories and an additional course from either subcategory.</p>
+                <h4>Va. Quantitative Literacy</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="ANTHRO 10A" />
+                </div>
+                <h4>Vb. Formal Reasoning</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="ANTHRO 10C" />
+                </div>
+                <h4>VI. Language Other Than English</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="SPANISH 1C" />
+                </div>
+                <h4>VI. Language Other Than English (1)</h4>
+                <p>Students must finish coursework equivalent to a third quarter of study in a language.</p>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="SPANISH 1C" />
+                </div>
+                <h4>VII. Multicultural Studies (1)</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="AFAM 40A" />
+                </div>
+                <h4>International/Global Issues (1)</h4>
+                {/* <button>Pick Courses</button> */}
+                <div className="cards-row">
+                  <Card text="ANTHRO 2A" />
+                </div>
+                <p> </p>
+                <p> </p>
               </div>
               
             ) : (
