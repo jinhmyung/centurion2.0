bdf8dbd39e1c4d9b21dc4924816448d94119805b
feit3 <feit3@uci.edu>
2024-06-09 00:41:11 -0700

    Drag&Drop&Save the tags for each year and each season

---
+++ uci_planner/app/(tabs)/Dropfield.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
+import Card from './Card';
+import { ItemTypes } from './Constants';
+
+// import './Yearcard.css'; // Import the CSS file
+
+const Dropfield = ({ onDrop, items } : any) => {
+
+  const [droppedItems, setDroppedItems] : any = useState([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+      accept: ItemTypes.CARD,
+      // drop: (item) => {
+      //   let t = droppedItems; 
+      //   t.push(onDrop(item)); 
+      //   setDroppedItems(t);
+      // },
+      drop: (item) => onDrop(item),
+      collect: (monitor) => ({
+          isOver: monitor.isOver(),
+      }),
+  }));
+
+  return (
+    <div 
+      ref={drop}
+      style={{
+        border: `${isOver ? '2px rgba(0, 0, 0, 0.2) dashed' : '2px rgba(0, 0, 0, 0.2) solid'}`,
+        borderRadius: 10,
+        minWidth: 150, 
+        minHeight: 50,
+        height: "100%",
+        alignItems: "center", 
+        display: "flex", 
+        flexDirection: "column"
+      }}
+    >
+      {items.map((item : any) => (
+        <div key={Math.random()}>
+          <Card text={item}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Dropfield;
--- uci_planner/app/(tabs)/Yearcard.tsx
+++ uci_planner/app/(tabs)/Yearcard.tsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import './Yearcard.css'; // Import the CSS file
+import Dropfield from './Dropfield';
+import Card from './Card';
 
 const Yearcard = ({ year } : any) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
+  };
+
+  const [droppedItems, setDroppedItems] : any = useState({
+    fall: [], 
+    winter: [], 
+    spring: [],
+    summer: [],
+  });
+
+  const handleDrop = (season : any, item : any) => {
+    let t = [...droppedItems[season]]; 
+    t.push(item.text); 
+    setDroppedItems({...droppedItems, [season]: t});
+  };
+
+  const handleRemoveItem = (index : any) => {
+      const updatedItems = [...droppedItems];
+      updatedItems.splice(index, 1);
+      setDroppedItems(updatedItems);
   };
 
   return (
@@ -18,10 +39,18 @@ const Yearcard = ({ year } : any) => {
       </div>
       {isExpanded && (
         <div className="content">
-          <span>Fall</span>
-          <span>Winter</span>
-          <span>Spring</span>
-          <span>Summer</span>
+          {['fall', 'winter', 'spring', 'summer'].map((season) => (
+            <div
+              key={Math.random()}
+              style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}
+            >
+              <span style={{ textAlign: 'center' }}>{season.charAt(0).toUpperCase() + season.slice(1)}</span>
+              <Dropfield
+                items={droppedItems[season]}
+                onDrop={(item:any) => handleDrop(season, item)}
+              />
+            </div>
+          ))}
         </div>
       )}
     </div>
